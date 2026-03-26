420111c5a16d67352dcc2a825de447fde35ebf35
David-C23155849Q <davidsithole2023@gmail.com>
2026-03-26 14:51:41 +0200

    Updated version

---
+++ canteen/templates/canteen/readme.m
@@ -0,0 +1,8 @@
+<!-- 🔥 MOVING BORDER GLOW -->
+        <div class="absolute inset-0 rounded-2xl pointer-events-none
+                    before:absolute before:inset-0 before:rounded-2xl
+                    before:bg-[linear-gradient(120deg,transparent,rgba(59,130,246,0.9),rgba(168,85,247,0.9),transparent)]
+                    before:bg-[length:300%_300%]
+                    before:animate-borderMove
+                    before:opacity-80">
+        </div>
